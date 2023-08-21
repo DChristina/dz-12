5fd4cca01cd31e48cd1826c262aa31db20443092
Christina <kristia1ewik@gmail.com>
2023-08-21 20:34:51 +0300

    Preparation coding

---
+++ src/main/java/Woman.java
@@ -0,0 +1,38 @@
+public class Woman extends Person{
+    private String maidenlastName;
+
+    public Woman(String firstName, String lastName, int age) {
+        super(firstName, lastName, age);
+        this.setMaidenlastName(lastName);
+    }
+    @Override
+    public void registerPartnership(Person newPartner){
+        if (getPartner() == null) {
+            this.partner = newPartner;
+            this.partner.partner = this;
+            this.setLastName(newPartner.getLastName());
+        }
+    }
+
+    @Override
+    public void  deregisterPartnership(Boolean divorceHappend){
+        if (divorceHappend){
+            this.partner.setPartner(null);
+            setPartner(null);
+            this.setLastName(getMaidenlastName());
+        }
+    }
+
+    @Override
+    public boolean isRetired() {
+        return (this.getAge()>=60);
+    }
+
+    public String getMaidenlastName() {
+        return maidenlastName;
+    }
+
+    public void setMaidenlastName(String maidenlastName) {
+        this.maidenlastName = maidenlastName;
+    }
+}
